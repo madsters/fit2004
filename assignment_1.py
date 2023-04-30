3189ad703339046c3762a1793c8d342e03a88e7e
maddynuni <110168661+maddynuni@users.noreply.github.com>
2023-04-30 12:54:05 +1000

    question 2 finished

--- assignment_1.py
+++ assignment_1.py
@@ -31,16 +31,20 @@ def optimalRoute(start, end, passengers, roads):
     for road in roads:
         if road[0] > max:
             max = road[0]
-    locations = range(max)
+    locations = range(max + 1)
 
     # create adjacency list | T: O(L), S: O(L + R)
     adjacency_list = get_adjacency_list(roads, locations)
     # add passenger flags to adjacency list | T: O(P + L), S: O(1)
     adjacency_list = add_passenger_flags(adjacency_list, passengers)
     # find shortest path | 
-    shortest_path = dijkstra_shortest_path(adjacency_list, start, end)
-
-    return shortest_path
+    distances, predecessors = dijkstra_shortest_path(adjacency_list, start)
+    path = []
+    while end:
+        path.append(end)
+        end = predecessors[end]
+    path.append(start)
+    return path[::-1]
 
 def get_adjacency_list(roads: tuple, locations: list) -> list:
     """
@@ -196,46 +200,57 @@ def select_sections(occupancy_probability):
     
     An auxillary function get_min(row) is used to convenietly return the index and value of the minimum in a row.
     """
-    running_total = [[0 for row in occupancy_probability] for col in occupancy_probability[0]]
+    running_total = [[0 for col in occupancy_probability[0]] for row in occupancy_probability]
     running_total[0] = occupancy_probability[0]
     # build matrix of running totals
-    for i, row in enumerate(occupancy_probability):
+    for i, row in enumerate(running_total):
         if i == 0:
             continue
         for j, col in enumerate(row):
-            options = [occupancy_probability[i-1][j-1], occupancy_probability[i-1][j], occupancy_probability[i-1][j+1]]
-            running_total[i][j] = col + min(options)
+            if j == 0:
+                options = [running_total[i-1][j], running_total[i-1][j+1]]
+            elif j == len(row) - 1:
+                options = [running_total[i-1][j-1], running_total[i-1][j]]
+            else: 
+                options = [running_total[i-1][j-1], running_total[i-1][j], running_total[i-1][j+1]]
+            running_total[i][j] = occupancy_probability[i][j] + min(options)
     # find minimum value in final row
-    min_index, min_value = get_min(running_total[-1])
+    min_index = running_total[-1].index(min(running_total[-1]))
     # backtrack through matrix
     path = []
     for i in range(len(running_total) - 1, -1, -1):
         # add index to path
         path.append((i, min_index))
         # find minimum value in adjacent cells in the row above
-        min_index, min_value = get_min(running_total[i-1][min_index-1], running_total[i-1][min_index], running_total[i-1][min_index+1])
+        min_index = get_min(running_total[i-1], min_index)
+    
+    path.reverse() # T: O(n)
     return path
 
-def get_min(row):
+def get_min(row, current_index):
     """
     Function description:
-    returns the index and value of the minimum in a row
+    returns the index of the minimum adjacent value in the row above the current row
 
     :Input:
         row: list of values
+        current_index: index of value of interest in previous row
     :Output:
         index: index of minimum value
-        value: minimum value
     :Time complexity: O(n)
     :Aux space complexity: O(1)
     """
     min_index = 0
-    min_value = row[0]
-    for i, value in enumerate(row):
-        if value < min_value:
-            min_index = i
-            min_value = value
-    return min_index, min_value
+    if current_index == 0:
+        min_value = min(row[0:min_index+1])
+        min_index = row.index(min_value)
+    if current_index == len(row) - 1:
+        min_value = min(row[min_index-1:len(row)])
+        min_index = row.index(min_value)
+    else:
+        min_value = min(row[current_index-1:current_index+2])
+        min_index = row.index(min_value)
+    return min_index
             
 """
 Helper classes
--- test_assignment_1.py
+++ test_assignment_1.py
@@ -8,7 +8,7 @@ locations = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 roads = [(0, 3, 4, 5), (3, 4, 4, 5), (3, 8, 23, 0), (4, 3, 2, 0), (2, 5, 1, 4), (8, 9, 23, 0)]
 
 ad_list = assignment_1.get_adjacency_list(roads, locations)
-print(ad_list)
+#print(ad_list)
 
 # adjacency list working
 
@@ -17,15 +17,46 @@ Test add passenger flags
 """
 passengers = [4]
 ad_list_passengers = assignment_1.add_passenger_flags(ad_list, passengers)
-print(ad_list_passengers)
+#print(ad_list_passengers)
 
 # passenger flags working
+
+"""
+TEST Q1
+"""
+
+# Example
+start = 0
+end = 4
+# The locations where there are potential passengers
+passengers = [2, 1]
+# The roads represented as a list of tuple
+roads = [(0, 3, 5, 3), (3, 4, 35, 15), (3, 2, 2, 2), (4, 0, 15, 10),
+(2, 4, 30, 25), (2, 0, 2, 2), (0, 1, 10, 10), (1, 4, 30, 20)]
+# Your function should return the optimal route (which takes 27 minutes).
+print(assignment_1.optimalRoute(start, end, passengers, roads))
+[0, 3, 2, 0, 3, 4]
 
 """
 Test shortest path
 """
 
-
-
 shortest_path = assignment_1.dijkstra_shortest_path(ad_list_passengers, 0)
-print(shortest_path)
+#print(shortest_path)
+
+"""
+Test select sections
+"""
+occupancy_probability = [
+[31, 54, 94, 34, 12],
+[26, 25, 24, 16, 87],
+[39, 74, 50, 13, 82],
+[42, 20, 81, 21, 52],
+[30, 43, 19, 5, 47],
+[37, 59, 70, 28, 15],
+[ 2, 16, 14, 57, 49],
+[22, 3, 9876, 19, 99]]
+
+#print(assignment_1.select_sections(occupancy_probability))
+
+# select sections working
