555358c9dd7c007aa7f4f93d628ea2be15b15c18
maddynuni <110168661+maddynuni@users.noreply.github.com>
2023-05-26 09:38:43 +1000

    good progress

--- assignment_2.py
+++ assignment_2.py
@@ -1,99 +1,143 @@
-def maxThroughput(connections, maxIn, maxOut, origin, targets):
-    # create_graphs is O(D^2 + C), however D^2 + C < C^2 (every data centre has at least one connection), so O(C^2) dominates
-    vertices, maxIn, maxOut, res_graph, rev_graph = create_graphs(connections, maxIn, maxOut, targets)
-    max_flow = 0
-    # super sinkkkkk
-    target = len(res_graph) - 1
-    min_cap, path = bfs(res_graph, maxIn, maxOut, origin, target)
-    # while there is a path from origin to target in residual graph
-    while path:
-        max_flow += min_cap
-        min_cap, path = bfs(res_graph, origin, targets)
-    pass
+"""
+CapacityFlowGraph class, has single source and super sink
+"""
+class CapacityFlowGraph:
+    def __init__(self, size, source, targets):
+        self.source = source
+        self.size = size
+        self.targets = targets
+        self.res_graph, self.rev_graph = self.__create_graphs(self.size)
+    
+    def __create_graphs(self, size):
+        # +2 for super sink, *3 for in, out
+        res_graph = [[0 for i in range(3*(size + 2))] for j in range(3*(size + 2))]
+        rev_graph = [[0 for i in range(3*(size + 2))] for j in range(3*(size + 2))]
 
-def bfs(res_graph, rev_graph, maxIn, maxOut, origin, target):
-    # returns list of tuples of path and graph traversed from origin to target (whichever one is found first)
-    # and minimum capacity along path
-    # graph traversed: 0 = res, 1 = rev
-    # if no path found, returns none
-    # graph is residual graph
-    # maxIn and maxOut are lists of lists of [vertex, max in/out]
-    # origin is vertex
-    # targets is list of vertices
-    # O(C^2)
+        # create super sink
+        for target in self.targets:
+            res_graph[-1][target] = float('inf')
+            rev_graph[target][-1] = float('inf') 
+            res_graph[-2][target] = float('inf')
+            rev_graph[target][-2] = float('inf')
+            res_graph[-3][target] = float('inf')
+            rev_graph[target][-3] = float('inf')
 
-    # initialise visited list
-    visited = []
-    parents = [[None] for i in range(len(res_graph))]
-    # initialise path list
-    path = []
-    # initialise min capacity
-    min_capacity = float('inf')
-    queue = [origin]
-    parent = None
+        return res_graph, rev_graph
 
-    def backtrack(parents):
-        # returns path from origin to target
-        # O(D)
-        # !! good chance this sucks
+    def add_edge(self, u, v, capacity, uout, vin):
+        u_index = self.vertex_to_index(u)
+        v_index = self.vertex_to_index(v)
+
+        # residual edge from u to v, flow = capacity
+        self.res_graph[u_index][v_index] = capacity
+        # reverse edge from v to u, flow = 0
+        self.rev_graph[v_index][u_index] = 0
+
+        # residual edge from u to uout, flow = uout, residual edge from vin to v, flow = vin
+        self.res_graph[u_index][u_index + 1] = uout
+        self.res_graph[v_index - 1][v_index] = vin
+        # reverse edge from uout to u, flow = 0, reverse edge from v to vin, flow = 0
+        self.rev_graph[u_index + 1][u_index] = 0
+        self.rev_graph[v_index][v_index - 1] = 0
+
+    def vertex_to_index(vertex):
+        return vertex*3 + 1
+    
+    def index_to_vertex(index):
+        return index//3
+    
+    def augment_path(self, path, flow):
+        # path is list of tuples of (u, v)
+        # min_capacity is int
+        for edge in path:
+            u = edge[0]
+            v = edge[1]
+            flow = edge[2]
+
+            # forward edge
+            if self.res_graph[u][v] >= flow:
+                self.res_graph[u][v] -= flow
+                self.rev_graph[v][u] += flow
+            # reverse edge
+            elif self.rev_graph[v][u] >= flow:
+                self.rev_graph[v][u] -= flow
+                self.res_graph[u][v] += flow
+            else:
+                raise ValueError("Invalid path")
+            
+        print('Success augmenting path with flow ', flow)
+
+    def find_path_bfs(self):
+        visited = []
+        parents = [[None] for i in range(len(self.res_graph))]
+        # initialise path list
         path = []
-        vertex = len(parents) - 1
-        while vertex != origin:
-            path.append((parents[vertex], vertex))
-            vertex = parents[vertex]
-        return path
+        # initialise min capacity
+        min_capacity = float('inf')
+        queue = [self.vertex_to_index(self.source)]
+        parent = None
 
+        def backtrack(parents):
+            # returns path from origin to target and min flow cap
+            # O(D)
+            # !! good chance this sucks
+            path = []
+            vertex = len(parents) - 1
 
-    # while queue is not empty
-    while next_vertex:
-        vertex = queue.pop()
+            min_flow = float('inf')
 
-        if vertex not in visited:
-            visited.append(vertex)
-            parents.append(parent)
+            while vertex != self.vertex_to_index(self.source):
+                path.append((parents[vertex], vertex))
+                if self.res_graph[parents[vertex]][vertex] < min_flow:
+                vertex = parents[vertex]
 
-            # end of path!
-            if vertex == target:
-                return min_capacity, path
-            
-            
-            # for each neighbour of vertex
-            for i in range(len(res_graph[vertex])):
-                if i not in visited:
-                    # valid path option! "forward edge"
-                    if res_graph[vertex][i] > 0 and maxIn:
-                        next_vertex = i
-                        path.append((vertex, next_vertex, 0))
-                        # get min flow
-                        if res_graph[vertex][i] < min_capacity:
-                            min_capacity = res_graph[vertex][i]
-                        break
-                    # valid path option! "reverse edge"
-                    elif rev_graph[vertex][i] > 0:
-                        next_vertex = i
-                        path.append((vertex, next_vertex, 0))
-                        # get min flow
-                        if rev_graph[vertex][i] < min_capacity:
-                            min_capacity = rev_graph[vertex][i]
-                        break
-                    else:
+            return path.reverse()
+
+        found_path = False
+
+        # while queue is not empty
+        while queue:
+            vertex = queue.pop()
+
+            if vertex not in visited:
+                visited.append(vertex)
+                parents[vertex] = parent
+
+                # end of path!
+                if vertex == len(self.res_graph) - 1:
+                    found_path = True
+                    break
+                
+                # for each neighbour of vertex
+                for i in range(len(self.res_graph[vertex])):
+                    if i not in visited:
+                        # valid path option! "forward edge"
+                        if self.res_graph[vertex][i] > 0:
+                            # queue i
+                            queue.append(i)
+                            # set parent for i
+                            parents[i] = vertex
+                        # valid path option! "reverse edge"
+                        elif self.rev_graph[vertex][i] > 0:
+                            # queue i
+                            queue.append(i)
+                            # set parent for i
+                            parents[i] = vertex
                         # edge full/no edge!
-                        continue
-            
-    return min_capacity, backtrack(parents)
+                        else:
+                            continue
+                
+
+        if not found_path:
+            return None, None
+        
+        return backtrack(parents)
 
 
-def create_graphs(connections, max_in, max_out, targets):
-    """
-    Function description: Creates a graph as a list of vertices and an adjacency list from the given connections, as 
-    well as a residual graph
-    Graphs include super sink
-    :Input:
-    connections: list of edges with max flow along the edge
-    :Output, return or postcondition: vertices: list of vertices, graph: adjacency list
-    :Time complexity: O(D^2 + C)
-    :Aux space complexity: O(D^2 + C)
-    """
+    
+
+
+def maxThroughput(connections, maxIn, maxOut, origin, targets):
     max = 0
     # find no. data centres O(C)
     for connection in connections:
@@ -101,25 +145,10 @@ def create_graphs(connections, max_in, max_out, targets):
             max = connection[0]
         if connection[1] > max:
             max = connection[1]
-    vertices = list(range(max + 1))
-    # create empty matrix with three nodes per data centre O(D^2), + 2*3 for super sink
-    res_graph = [[0 for i in range(3*(max + 2))] for j in range(3*(max + 2))]
-    rev_graph = [[0 for i in range(3*(max + 2))] for j in range(3*(max + 2))]
-    # create adjacency matrix O(C)
-    i = 0
-    for connection in connections:
-        # residual edge from u to v, flow = capacity
-        res_graph[connection[0]][connection[1]] = connection[2]
-        # reverse edge from v to u, flow = 0
-        rev_graph[connection[1]][connection[0]] = 0
-    # create edges from super sink to targets O(D)
-    for target in targets:
-        res_graph[-1][target] = float('inf')
-        rev_graph[target][-1] = float('inf')
-        res_graph[-2][target] = float('inf')
-        rev_graph[target][-2] = float('inf')
-        res_graph[-3][target] = float('inf')
-        rev_graph[target][-3] = float('inf')
-
-    return vertices, max_in_new, max_out_new, res_graph, rev_graph
     
+    graph = CapacityFlowGraph(max, origin, targets)
+
+    # create adjacency matrix O(C)
+    for connection in connections:
+        graph.add_edge(connection[0], connection[1], connection[2], maxIn[connection[0]], maxOut[connection[1]])
+
